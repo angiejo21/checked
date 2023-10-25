4576e974c47457dddc50560b71cf403d063f1448
Angela Bellò <angela.bello90@gmail.com>
2023-10-25 12:46:17 +0200

    + basic structure and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ToDoList />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return (
+    <header className="header">
+      <h1>Checked</h1>
+      <FromNewToDo />
+    </header>
+  );
+}
+
+function FromNewToDo() {
+  return (
+    <form className="form--new-to-do">
+      <input type="text" placeholder="What do you need to do?" />
+      <button>➕</button>
+    </form>
+  );
+}
+
+function ToDoList() {
+  return (
+    <div className="to-do-list">
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+    </div>
+  );
+}
+
+function ToDoItem() {
+  return (
+    <div className="to-do-item">
+      <div className="to-do-item__main">
+        <input type="checkbox" />
+        <h3 className="to-do-item__title">Thing to do</h3>
+        <div className="icons">📅 📌 🏷️</div>
+      </div>
+      <ToDoItemForm />
+    </div>
+  );
+}
+
+function ToDoItemForm() {
+  return (
+    <form className="form--edit-to-do">
+      <div className="date">
+        <label>📅</label>
+        <input type="date" />
+        <input type="time" />
+      </div>
+      <div className="place">
+        <label>📌</label>
+        <input type="text" />
+      </div>
+      <div className="category">
+        <label>🏷️</label>
+        <select>
+          <option>Work</option>
+          <option>Health</option>
+          <option>Family</option>
+          <option>Sport</option>
+          <option>Finance</option>
+          <option>Travel</option>
+          <option>Leisure</option>
+        </select>
+      </div>
+      <button>✏️</button>
+    </form>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <select>
+        <option>Work</option>
+        <option>Health</option>
+        <option>Family</option>
+        <option>Sport</option>
+        <option>Finance</option>
+        <option>Travel</option>
+        <option>Leisure</option>
+      </select>
+      <p>© Angela Bellò 2023</p>
+    </footer>
+  );
+}
